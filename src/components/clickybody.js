7b1b1c8b5e591778c6cbe10922550374d6a8336b
Tamami <tamami_polk@hotmail.com>
2019-07-19 18:26:29 -0500

    Testing Heroku

--- src/components/clickybody.js
+++ src/components/clickybody.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ClickyBody extends Component {
     render() {
         return (
-            <div>Clicky Body</div>
+            <div>Clicky Body Here</div>
         )
     }
 };
