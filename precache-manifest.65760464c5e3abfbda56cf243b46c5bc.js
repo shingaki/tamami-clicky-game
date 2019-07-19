a14d945bc775fc9b1bbbf126415c904ed634b027
Tamami <tamami_polk@hotmail.com>
2019-07-19 17:22:24 -0500

    Updates

---
+++ precache-manifest.65760464c5e3abfbda56cf243b46c5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a684a82b85d5d9946f1097b08970cf8e",
+    "url": "/tamami-clicky-game/index.html"
+  },
+  {
+    "revision": "38ff68b2d3e11d9f617e",
+    "url": "/tamami-clicky-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/tamami-clicky-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "38ff68b2d3e11d9f617e",
+    "url": "/tamami-clicky-game/static/js/main.2cc57c39.chunk.js"
+  },
+  {
+    "revision": "4f8da2b637e6c7ae7e18",
+    "url": "/tamami-clicky-game/static/js/runtime~main.c06a1983.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tamami-clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tamami-clicky-game/precache-manifest.65760464c5e3abfbda56cf243b46c5bc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tamami-clicky-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
